e92cae1837a3a6afbaf30fb3b840cb4dbde6bc55
Zshww <2778069383@qq.com>
2026-02-26 11:42:10 +0800

    gitt.java

--- src/main/java/com/ithezsh/git/gitt.java
+++ src/main/java/com/ithezsh/git/gitt.java
@@ -11,6 +11,6 @@ public class gitt {
           System.out.println("hello zsh");
           System.out.println("hello zsh2");
               System.out.println("hello git");
-        System.out.println("hello git2");
+        System.out.println("hello g");
     }
 }
